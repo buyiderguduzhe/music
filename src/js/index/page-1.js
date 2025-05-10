b633513258d3a3608de77daef337e69b0880a4ca
buyiderguduzhe <cdsm915754957@qq.com>
2025-05-10 10:16:45 +0800

    目录修改

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -34,7 +34,7 @@
         },
         loadModule1() {
             let script1 = document.createElement('script')
-            script1.src = '../js/index/page-1-1.js'
+            script1.src = './js/index/page-1-1.js'
             script1.onload = function () {
                 console.log('模块一加载完毕')
             }
@@ -43,7 +43,7 @@
         },
         loadModule2() {
             let script2 = document.createElement('script')
-            script2.src = '../js/index/page-1-2.js'
+            script2.src = './js/index/page-1-2.js'
             script2.onload = function () {
                 console.log('模块2加载完毕')
             }
