41d78039ca9d7d94ec7c9e2dd260497f60cef209
buyiderguduzhe <cdsm915754957@qq.com>
2025-05-16 15:14:21 +0800

    歌单页面

--- docs/src/js/index/playlist.js
+++ docs/src/js/index/playlist.js
@@ -2,34 +2,36 @@
     let view = {
         el: 'section.songs',
         template: `
-        <li>
-          <div>
-            <h3>{{song.name}}</h3>
-            <p>
-              <svg class="icon icon-sq">
-                <use xlink:href="#icon-sq"></use>
-              </svg>
-              {{song.singer}}
-            </p>
-          </div>
-          <a class="playButton" href="./song.html?id={{song.id}}">
-            <svg class="icon icon-play">
-              <use xlink:href="#icon-play"></use>
-            </svg>
-          </a>
-        </li>
+            <li>
+              <h3>{{song.name}}</h3>
+              <p>
+                <svg class="icon icon-sq">
+                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
+                </svg>
+                {{song.singer}}
+              </p>
+              <a class="playButton" href="./song.html?id={{song.id}}">
+                <svg class="icon icon-play">
+                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+                </svg>
+              </a>
+            </li>
       `,
         init() {
             this.$el = $(this.el);
         },
         render(data) {
+            // 设置封面图片和简介
+            document.getElementById('playlistCover').src = data.coverImageUrl || '';
+            document.getElementById('playlistIntroduction').innerText = data.introduction || '';
+
             // 清空旧内容并渲染
             const $list = this.$el.find('ol.list').empty();
 
             data.songs.forEach((song) => {
                 let $li = $(this.template
                     .replace('{{song.name}}', song.name)
-                    .replace('{{song.singer}}', song.singer)
+                    .replace('{{song.singer}}', song.singer || '') // 如果没有歌手则为空字符串
                     .replace('{{song.id}}', song.id)
                 );
                 $list.append($li);
@@ -39,19 +41,32 @@
 
     let model = {
         data: {
-            songs: []
+            songs: [],
+            coverImageUrl: '',
+            introduction: ''
         },
         find(playlistId) {
             // LeanCloud 查询条件：playlist 字段等于传入的 id（字符串）
             const query = new AV.Query('Song');
             query.equalTo('playlist', String(playlistId)); // 确保类型一致为 string
-            return query.find().then(songs => {
+
+            // 另外查询歌单封面图片URL和简介
+            const coverQuery = new AV.Query('Song');
+            coverQuery.equalTo('playlist', String(playlistId));
+            coverQuery.exists('picture'); // 只选择存在 picture 字段的记录
+            coverQuery.limit(1); // 只需要一条记录来获取封面图片URL和简介
+
+            return AV.Promise.all([query.find(), coverQuery.first()]).then(([songs, coverSong]) => {
                 // 把 LeanCloud 返回的数据整理成更方便使用的格式
                 this.data.songs = songs.map(song => ({
                     id: song.id,
                     ...song.attributes
                 }));
-                return songs;
+                if (coverSong) {
+                    this.data.coverImageUrl = coverSong.attributes.picture;
+                    this.data.introduction = coverSong.attributes.introduction;
+                }
+                return { songs, coverSong };
             }).catch(error => {
                 console.error('查询歌曲失败:', error);
             });
